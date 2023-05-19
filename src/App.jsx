7664669e2f16d891581755a45b88fc776849339f
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-05-19 13:40:44 +0700

    update container navbar, footer add filter bookmarks

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ const App = () => {
             className={`${
               menu
                 ? "-right-full md:hidden"
-                : "right-0 md:top-full md:-translate-y-5 md:-translate-x-1 lg:-translate-x-9"
-            } fixed top-0 w-fit h-screen px-4 py-6 bg-white shadow-2xl transition-all duration-500 z-50 md:absolute md:h-fit md:px-3 md:rounded-lg md:shadow-2xl dark:bg-dark`}
+                : "right-0 md:top-full md:-translate-y-6 md:-translate-x-1 lg:-translate-x-28"
+            } fixed top-0 w-fit h-screen px-4 py-6 bg-white shadow-xl border border-profile dark:border-grey/30 transition-all duration-500 z-50 md:absolute md:h-fit md:px-3 md:rounded-lg dark:bg-dark`}
           >
             <Navbar handle={handleMenu} />
           </nav>
@@ -27,7 +27,7 @@ const App = () => {
           <Route path="/projects" element={<Projects />} />
           <Route path="/bookmarks" element={<Bookmarks />} />
         </Routes>
-        <div className="container px-8 pb-10 md:pb-5 lg:pb-8 lg:px-10 xl:px-12">
+        <div className="container px-8 pb-10 md:pb-5 lg:pb-8 lg:px-10 xl:px-28">
           <div className="flex flex-col gap-5 pt-6 border-t border-profile md:gap-4 lg:flex-row lg:justify-between lg:items-end lg:gap-0 dark:border-grey/50">
             <Footer />
           </div>
--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -17,15 +17,17 @@ const Bookmarks = () => {
     "quotes",
   ];
 
-  const [activeTag, setActiveTag] = useState("inspirations");
-  const tagHandler = (e) => {
+  const [activeTag, setActiveTag] = useState("images");
+  const tagHandle = (e) => {
     setActiveTag(e.target.innerText.toLowerCase());
   };
 
+  const filteredBookmarks = bookmarks.filter((bookmark) => bookmark.tag.find((el) => el === activeTag))
+  
   useEffect(() => {
     document.title = "Hibatillah's Bookmarks";
-    console.log(bookmarks);
-  }, []);
+    console.log("🚀 ~ file: Bookmarks.jsx:26 ~ Bookmarks ~ filteredBookmarks:", filteredBookmarks)
+  }, [filteredBookmarks]);
 
   return (
     <main className="container space-y-14">
@@ -45,10 +47,10 @@ const Bookmarks = () => {
             <li key={index} className="cursor-pointer">
               <Tag
                 tag={tag}
-                handle={tagHandler}
+                handle={tagHandle}
                 custom={`${
                   tag === activeTag
-                    ? "shadow-xl text-white bg-dark dark:bg-white dark:text-dark dark:shadow-grey/10"
+                    ? "text-white bg-dark dark:text-black dark:bg-white shadow-xl dark:shadow-grey/10"
                     : "text-dark bg-white dark:bg-dark dark:text-white"
                 } scale-90`}
               />
@@ -60,7 +62,7 @@ const Bookmarks = () => {
         id="bookmarks"
         className="grid grid-cols-1 gap-y-6 md:grid-cols-2 md:gap-x-5 xl:grid-cols-3 lg:gap-x-4"
       >
-        {bookmarks.map((bookmark, index) => (
+        {filteredBookmarks.map((bookmark, index) => (
           <li key={index}>
             <a href={bookmark.link} target="_blank" rel="noopener noreferrer">
               <BookmarkCard bookmark={bookmark} />
