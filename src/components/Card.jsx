9e9b9bc00d963d26ce498ad1b0e56f8d999e2f3c
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-06-04 23:08:06 +0700

    update icon project card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Tag from "./Tag";
-import { codeIcon } from "../assets";
 
 export const ProjectCard = ({ project }) => {
   const { thumbnail, title, tag, source } = project;
-  
+
   return (
     <div className="project-card group/project">
       <img
@@ -40,7 +39,14 @@ export const ProjectCard = ({ project }) => {
           className="flex-none"
         >
           <div className="px-2 py-1.5 border border-grey hover:bg-profile/50 rounded-md">
-            <img src={codeIcon} alt="source" className="w-5 h-5 text-dark" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 640 512"
+              className="w-5 h-5 fill-dark dark:fill-white"
+            >
+              <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z" />
+            </svg>
           </div>
         </a>
       </div>
